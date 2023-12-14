1b0782826338bcf257f0ffa6b4fbdb799d15d073
Felix Vadcard <vadcard.felix@gmail.com>
2023-12-14 11:31:46 +0100

    adding profile to controle test and spring profile by maven profiles

--- application/src/test/java/fef/vad/www/app/ApplicationTest.java
+++ application/src/test/java/fef/vad/www/app/ApplicationTest.java
@@ -1,15 +1,13 @@
 package fef.vad.www.app;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ApplicationTest {
 
   @Test
-  void contextLoads() {}
-
+  void testTheTest() {
+    assertThat(1 + 1).isEqualTo(2);
+  }
 }
