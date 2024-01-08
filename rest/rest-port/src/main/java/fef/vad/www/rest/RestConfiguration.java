6d56056662f00aa2eb2031bc4c36a2e564eeab3c
Felix Vadcard <vadcard.felix@gmail.com>
2024-01-08 16:02:24 +0100

    adding origin used by local dev

--- rest/rest-port/src/main/java/fef/vad/www/rest/RestConfiguration.java
+++ rest/rest-port/src/main/java/fef/vad/www/rest/RestConfiguration.java
@@ -15,6 +15,7 @@ public class RestConfiguration {
         registry.addMapping("/mail").allowedOrigins(
           "http://localhost:5000",
           "http://localhost:5173",
+          "http://127.0.0.1:5173",
           "https://thecatmaincave.com"
         );
       }
