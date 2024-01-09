f047e208ee554339bd1587befea90096745aca89
Felix Vadcard <vadcard.felix@gmail.com>
2024-01-09 09:58:47 +0100

    Loging SMTP port to use

--- mail/mail-port/src/test/java/fef/vad/www/mail/TestConfiguration.java
+++ mail/mail-port/src/test/java/fef/vad/www/mail/TestConfiguration.java
@@ -1,10 +1,12 @@
 package fef.vad.www.mail;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+@Slf4j
 @Configuration
 public class TestConfiguration {
 
@@ -13,6 +15,7 @@ public class TestConfiguration {
 
   @PostConstruct
   private void initJavaMailSender() {
+    log.info("port to use : {}", AppGreenMailExtension.greenMail.getSmtp().getPort());
     javaMailSender.setPort(AppGreenMailExtension.greenMail.getSmtp().getPort());
   }
 }
