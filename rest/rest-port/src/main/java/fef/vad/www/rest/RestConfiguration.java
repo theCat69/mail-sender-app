b7039db61f3b1931be7f882c14edf0b8df28ab12
Felix Vadcard <vadcard.felix@gmail.com>
2024-01-16 10:46:39 +0100

    changing cors

--- rest/rest-port/src/main/java/fef/vad/www/rest/RestConfiguration.java
+++ rest/rest-port/src/main/java/fef/vad/www/rest/RestConfiguration.java
@@ -13,8 +13,8 @@ public class RestConfiguration {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/mail").allowedOrigins(
-          "http://127.0.0.1:5500",
-          "http://127.0.0.1:5173",
+          "http://localhost:5500",
+          "http://localhost:5173",
           "https://thecatmaincave.com"
         );
       }
